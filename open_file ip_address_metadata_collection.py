5bba1e9c2da601091e1b81780bfa848c75aa746e
M.E.C. <parapluiecorporation@protonmail.com>
2026-01-02 09:19:22 +0100

    Create ip_address_metadata_collection.py with structure

---
+++ open_file ip_address_metadata_collection.py
@@ -0,0 +1,25 @@
+"""
+OUTPUTS THE NETWORK NODE LOCATIONS OF NETWORK CONNECTED FILES.
+"""
+
+class IP_METADATA:
+  pass
+
+class INPUT_ARGS:
+  pass
+
+
+def args_parsing():
+  pass
+
+def collect_open_file_ips():
+  pass
+
+def ip_address_identification():
+  pass
+
+def ip_address_geoidentification():
+  pass
+
+def ip_address_metadata_collection():
+  pass
