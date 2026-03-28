5c3263bd7a9140fed5e23efad951205fd4e3a3b0
M.E.C. <parapluiecorporation@protonmail.com>
2026-03-28 18:35:01 +0100

    Update eu_central_us_central.sh

--- eu_central_us_central.sh
+++ eu_central_us_central.sh
@@ -1,4 +1,8 @@
 # transfer to python or go script and analyze ips from ping and traceroute (plus dig)
+# traceroute for all network node hops:
 traceroute speedtest.dallas.linode.com;
+# for DNS servers:
+dig speedtest.dallas.linode.com;
+# for connection latency:
 ping speedtest.dallas.linode.com;
 
